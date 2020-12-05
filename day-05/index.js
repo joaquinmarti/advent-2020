6e72d388be3834f2b3bb69e21d36ac193c7a9ab6
Joaquin Marti <joaquin@hanzo.es>
2020-12-05 07:52:48 +0100

    Day 5

---
+++ day-05/index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+function readBoardingPasses() {
+  return new Promise((resolve) => {
+    fs.readFile(`./input.txt`, 'utf8', function (err, data) {
+      resolve(data.split('\n'));
+    })
+  })
+}
+
+//
+
+function sliceBP(bp) {
+  return [bp.slice(0, 7), bp.slice(-3)];
+}
+
+function calcID(row, column) {
+  const rowNumber = parseInt(row.replace(/F/g, '0').replace(/B/g, '1'), 2);
+  const columnNumber = parseInt(column.replace(/R/g, '1').replace(/L/g, '0'), 2);
+  return rowNumber * 8 + columnNumber;
+}
+
+//
+
+function calcResultDay5A(bps) {
+  return bps.reduce((highest, bp) => {
+    const [row, column] = sliceBP(bp);
+    const id = calcID(row, column);
+    return id > highest ? id : highest;
+  }, 0);
+}
+
+function calcResultDay5B(bps) {
+  const ids = bps.map((bp) => {
+    const [row, column] = sliceBP(bp);
+    return calcID(row, column);
+  }).sort((a, b) => a - b);
+
+  return ids.reduce((missingID, id, index, ids) => id - ids[index - 1] > 1 ? id - 1 : missingID, 0);
+}
+
+async function start() {
+  const bps = await readBoardingPasses();
+  console.log(calcResultDay5A(bps));
+  console.log(calcResultDay5B(bps));
+}
+
+start();
