567c8830e47772d2f08914b33fd67caef8cd538f
Joaquin Marti <joaquin@hanzo.es>
2020-12-05 10:21:03 +0100

    Reorder replace methods

--- day-05/index.js
+++ day-05/index.js
@@ -16,7 +16,7 @@ function sliceBP(bp) {
 
 function calcID(row, column) {
   const rowNumber = parseInt(row.replace(/F/g, '0').replace(/B/g, '1'), 2);
-  const columnNumber = parseInt(column.replace(/R/g, '1').replace(/L/g, '0'), 2);
+  const columnNumber = parseInt(column.replace(/L/g, '0').replace(/R/g, '1'), 2);
   return rowNumber * 8 + columnNumber;
 }
 
