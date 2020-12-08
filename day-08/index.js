03545cc4405cad34cc8dcb6a0ed5ad75dffb9c6a
Joaquin Marti <joaquin@hanzo.es>
2020-12-08 10:05:44 +0100

    Day 8

---
+++ day-08/index.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+
+function readInput() {
+  return new Promise((resolve) => {
+    fs.readFile(`./input.txt`, 'utf8', function (err, data) {
+      resolve(data.split('\n').map((i) => {
+        const [name, parameter] = i.split(' ');
+        return {
+          name,
+          parameter
+        }
+      }));
+    })
+  });
+}
+
+//
+
+function calcAcc(input) {
+  const commandsExecuted = [];
+  let pointer = 0;
+  let acc = 0;
+
+  function excuteCommand(command) {
+    if (command) {
+      switch (command.name) {
+        case 'jmp':
+          pointer += parseInt(command.parameter);
+          break;
+        case 'acc':
+          acc += parseInt(command.parameter);
+        default:
+          pointer += 1;
+      }
+    }
+  }
+
+  while (!~commandsExecuted.indexOf(pointer)) {
+    commandsExecuted.push(pointer)
+    excuteCommand(input[pointer]);
+  }
+
+  return [
+    acc,
+    pointer - 1
+  ];
+}
+
+function changeCommand(input) {
+  let changed = -1;
+
+  return function () {
+    const commands = input.map(c => ({...c})); // Clone array avoiding references to objects
+
+    const replaceCommand = {
+      'nop': 'jmp',
+      'jmp': 'nop',
+    };
+
+    const indexToChange = commands.findIndex((i, index) => {
+      return ((i.name === 'nop' && i.parameter !== '+0') || i.name === 'jmp') && index > changed;
+    });
+
+    if (~indexToChange) {
+      commands[indexToChange].name = replaceCommand[commands[indexToChange].name];
+      changed = indexToChange;
+    }
+
+    return commands;
+  }
+}
+
+//
+
+function calcResultA(input) {
+  return calcAcc(input)[0];
+}
+
+function calcResultB(input) {
+  const commandIndexToTerminate = input.length - 1;
+  const executeCommandsChange = changeCommand(input);
+  let lastCommand = -1;
+  let acc = 0;
+
+  while (lastCommand !== commandIndexToTerminate) {
+    [acc, lastCommand] = calcAcc(executeCommandsChange());
+  }
+
+  return acc;
+}
+
+async function start() {
+  const input = await readInput();
+  console.log(calcResultA(input));
+  console.log(calcResultB(input));
+}
+
+start();
