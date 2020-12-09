c84f8cc6948287304bf60303b01ea776f0f68ac3
Joaquin Marti <joaquin@hanzo.es>
2020-12-09 07:15:33 +0100

    Day 9

---
+++ day-09/index.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+function readInput() {
+  return new Promise((resolve) => {
+    fs.readFile(`./input.txt`, 'utf8', function (err, data) {
+      resolve(data.split('\n').map((n) => parseInt(n)));
+    })
+  })
+}
+
+//
+
+function canSum(preamble, number) {
+  return !preamble.every(x => !~preamble.indexOf(number - x));
+}
+
+function sumArray(numbers) {
+  return numbers.reduce((total, number) => total + number);
+};
+
+//
+
+const PREAMBLE_SIZE = 25;
+
+function calcResultA(input) {
+  for (let i = PREAMBLE_SIZE; i < input.length; i++) {
+    const number = input[i];
+
+    if (!canSum(input.slice(i - PREAMBLE_SIZE, i), number)) {
+      return number;
+    }
+  }
+}
+
+function calcResultB(input) {
+  const numberToFind = calcResultA(input);
+  const result = [];
+
+  initialLoop: for (let x = 0; x < input.length; x++) {
+    contigiousLoop: for (let y = x + 1; y < input.length; y++) {
+      let numbersToSum = input.slice(x, y);
+      let sumContigious = sumArray(numbersToSum);
+
+      if (sumContigious > numberToFind) {
+        break contigiousLoop;
+      }
+
+      if (sumContigious === numberToFind) {
+        result.push(...numbersToSum);
+        break initialLoop;
+      }
+    }
+  }
+
+  //
+  result.sort((a, b) => a - b);
+
+  return result[0] + result[result.length - 1];
+}
+
+async function start() {
+  const input = await readInput();
+  console.log(calcResultA(input));
+  console.log(calcResultB(input));
+}
+
+start();
