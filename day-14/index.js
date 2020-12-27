b04f38d2b3d5d38d8864f271193bf00bf7f27bfc
Joaquin Marti <joaquin@hanzo.es>
2020-12-27 14:24:09 +0100

    Day 14 part 1

--- day-14/index.js
+++ day-14/index.js
@@ -1,17 +1,52 @@
+const { log } = require('console');
 const fs = require('fs');
 
 function readInput() {
   return new Promise((resolve) => {
     fs.readFile(`./input.txt`, 'utf8', function (err, data) {
-      resolve(data.split('\n').map((i) => [i[0], parseInt(i.slice(1, i.length))]));
+      resolve(data.split('\n'));
     })
   })
 }
 
+function processValue(value, mask) {
+  let binaryValue = String('000000000000000000000000000000000000' + value.toString(2)).slice(-36).split('');
+
+  [...mask].forEach((char, index) => {
+    if (char === '0' || char === '1') {
+      binaryValue[index] = char;
+    }
+  });
+
+  return parseInt(binaryValue.join(''), 2);
+}
 
 //
 
 function calcResultA(input) {
+  const mem = [];
+  let mask = '';
+
+  const maskStart = 'mask = ';
+  const memStart = 'mem[';
+
+  input.forEach((line) => {
+    const isLine = line.indexOf(maskStart);
+    const isMask = line.indexOf(memStart);
+
+    if (isLine === 0) {
+      mask = line.substr(maskStart.length);
+    }
+
+    if (isMask === 0) {
+      let address = line.substring(memStart.length, line.indexOf(']'));
+      let value = +line.substr(line.indexOf('=') + 2);
+
+      mem[address] = processValue(value, mask);
+    }
+  });
+
+  return mem.reduce((total, value) => total + value, 0);
 }
 
 function calcResultB(input) {
